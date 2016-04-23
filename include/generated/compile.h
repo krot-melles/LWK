d772261b642d8d52da1b308ca6869d5fee7e13cd
LWK <lwk-i9500@ubuntu.(none)>
2016-04-23 06:47:27 -0700

    initial commit

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 8 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#8 SMP PREEMPT Sat Apr 23 06:18:55 PDT 2016"
+#define LINUX_COMPILE_BY "lwk-i9500"
+#define LINUX_COMPILE_HOST "ubuntu"
+#define LINUX_COMPILER "gcc version 4.9.x-sabermod 20160216 (release) (GCC) "
