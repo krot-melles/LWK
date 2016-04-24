07cb01386af83e8bd8cd1ad5a4dc43a9dde9d878
LWK <lwk-i9500@ubuntu.(none)>
2016-04-24 01:46:59 -0700

    Linux 3.4.60

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 11 */
+/* This file is auto generated, version 12 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#11 SMP PREEMPT Sat Apr 23 23:07:39 PDT 2016"
+#define UTS_VERSION "#12 SMP PREEMPT Sun Apr 24 01:29:54 PDT 2016"
 #define LINUX_COMPILE_BY "lwk-i9500"
 #define LINUX_COMPILE_HOST "ubuntu"
 #define LINUX_COMPILER "gcc version 4.9.x-sabermod 20160216 (release) (GCC) "
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "3.4.55LWK-7"
+#define UTS_RELEASE "3.4.60LWK-7"
