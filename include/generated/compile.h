8ba265d00b3655b1ef22cd4d3b8f60b90061ea03
LWK <lwk-i9500@ubuntu.(none)>
2016-04-25 05:43:36 -0700

    back to -O2 (due to compile issues)

--- include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 18 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#18 SMP PREEMPT Sun Apr 24 04:54:01 PDT 2016"
-#define LINUX_COMPILE_BY "lwk-i9500"
-#define LINUX_COMPILE_HOST "ubuntu"
-#define LINUX_COMPILER "gcc version 4.9.x-sabermod 20160216 (release) (GCC) "
