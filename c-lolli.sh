5ce267ebe889d877c26b524ac0aa85db6e842085
Asad007 <alviasad2@gmail.com>
2016-05-02 07:19:41 -0700

    Make things easy 1) c-lolli.sh => compile lollipop version 2) c-marsh.sh => compile marshmallow version

---
+++ c-lolli.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# Kernel Compile Script for Lollipop
+
+# Cleanup first
+make clean && make mrproper;
+
+# defconfig !
+make lwkll_defconfig;
+
+# Compile Kernel
+make -j5 ARCH=arm SUBARCH=arm CROSS_COMPILE="../sabermod/bin/arm-linux-androideabi-";
