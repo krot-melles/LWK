5dbef5c617a5422ad6a480b5881e5c0a83c9b5c1
LWK <lwk@ubuntu.(none)>
2016-05-23 11:13:07 -0700

    Stocklike hotplug and Removed useless stuff :P

--- drivers/cpufreq/cpufreq_alucard.c
+++ drivers/cpufreq/cpufreq_alucard.c
@@ -83,14 +83,14 @@ static struct alucard_tuners {
 	atomic_t pump_dec_step;
 } alucard_tuners_ins = {
 	.sampling_rate = ATOMIC_INIT(60000),
-	.inc_cpu_load_at_min_freq = ATOMIC_INIT(60),
-	.inc_cpu_load = ATOMIC_INIT(80),
+	.inc_cpu_load_at_min_freq = ATOMIC_INIT(80),
+	.inc_cpu_load = ATOMIC_INIT(95),
 	.dec_cpu_load_at_min_freq = ATOMIC_INIT(40),
 	.dec_cpu_load = ATOMIC_INIT(60),
 #ifdef CONFIG_CPU_EXYNOS5410
 	.freq_responsiveness = ATOMIC_INIT(600000),
 #else
-	.freq_responsiveness = ATOMIC_INIT(800000),
+	.freq_responsiveness = ATOMIC_INIT(600000),
 #endif
 	.pump_inc_step = ATOMIC_INIT(1),
 	.pump_dec_step = ATOMIC_INIT(2),
