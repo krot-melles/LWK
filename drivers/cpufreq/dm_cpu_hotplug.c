5cb14e9fc5af9ca33b34396e6df97e7b120a3968
Asad <alviasad2@gmail.com>
2016-05-26 00:46:16 +0530

    enable screen on hotplug

--- drivers/cpufreq/dm_cpu_hotplug.c
+++ drivers/cpufreq/dm_cpu_hotplug.c
@@ -26,7 +26,7 @@
 
 #define	COLD_THRESHOLD	20
 #define NORMALMAX_FREQ	1600000
-#define NORMALMIN_FREQ	900000
+#define NORMALMIN_FREQ	400000
 #define POLLING_MSEC	50
 
 struct cpu_load_info {
@@ -54,7 +54,7 @@ static unsigned int delay = POLLING_MSEC;
 
 static bool exynos_dm_hotplug_disable;
 
-int screen_on_hotplug = 0;
+int screen_on_hotplug = 1;
 module_param(screen_on_hotplug, int, 0660);
 
 int hotplug_freq = NORMALMIN_FREQ;
