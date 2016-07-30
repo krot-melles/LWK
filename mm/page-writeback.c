3332904a2f87ffa0dd0675b6fb21240aafb33ca3
asad007 <alviasad2@gmail.com>
2016-07-30 09:38:02 -0700

    Some Defaults

--- mm/page-writeback.c
+++ mm/page-writeback.c
@@ -106,7 +106,7 @@ EXPORT_SYMBOL_GPL(dirty_writeback_interval);
 /*
  * The longest time for which data is allowed to remain dirty
  */
-#define DEFAULT_DIRTY_EXPIRE_INTERVAL 500 /* centiseconds */
+#define DEFAULT_DIRTY_EXPIRE_INTERVAL 600 /* centiseconds */
 #define DEFAULT_SUSPEND_DIRTY_EXPIRE_INTERVAL 12000 /* centiseconds */
 unsigned int dirty_expire_interval,
 	resume_dirty_expire_interval;
