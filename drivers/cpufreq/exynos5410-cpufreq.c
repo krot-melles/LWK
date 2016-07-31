ceeca1b3f846dd0cf1d911affac8ff8e91d6b89a
asad007 <alviasad2@gmail.com>
2016-07-30 18:28:24 -0700

    Remove 2GHZ freq Unstable Also try to boot with 1.9GHZ

--- drivers/cpufreq/exynos5410-cpufreq.c
+++ drivers/cpufreq/exynos5410-cpufreq.c
@@ -838,7 +838,9 @@ static void __init set_volt_table_CA15(void)
 				exynos5410_volt_table_CA15[i]);
 	}
 
-	max_support_idx_CA15 = L0;
+	exynos5410_freq_table_CA15[L0].frequency = CPUFREQ_ENTRY_INVALID;
+
+	max_support_idx_CA15 = L1;
 
 	exynos5410_freq_table_CA15[L14].frequency = CPUFREQ_ENTRY_INVALID;
 	exynos5410_freq_table_CA15[L15].frequency = CPUFREQ_ENTRY_INVALID;
