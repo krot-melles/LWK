8202069875f747c471ea6cbb4cb180be368c8c0a
asad007 <alviasad2@gmail.com>
2016-07-30 17:36:43 -0700

    Allow Overclock and Underclock

--- drivers/cpufreq/exynos-ikcs-cpufreq.c
+++ drivers/cpufreq/exynos-ikcs-cpufreq.c
@@ -844,9 +844,7 @@ static ssize_t show_freq_table(struct kobject *kobj,
 	int i, count = 0;
 
 	for (i = 0; merge_freq_table[i].frequency != CPUFREQ_TABLE_END; i++) {
-		if (merge_freq_table[i].frequency != CPUFREQ_ENTRY_INVALID && 
-			merge_freq_table[i].frequency <= 1600000 &&
-			merge_freq_table[i].frequency >= 250000)
+		if (merge_freq_table[i].frequency != CPUFREQ_ENTRY_INVALID)
 			count += sprintf(&buf[count], "%d ",
 				merge_freq_table[i].frequency);
         }
@@ -981,7 +979,7 @@ static struct attribute *iks_attributes[] = {
 
 static struct attribute_group iks_attr_group = {
 	.attrs = iks_attributes,
-	.name = "iks-cpufreq",
+	.name = "ikcs-cpufreq",
 };
 
 
