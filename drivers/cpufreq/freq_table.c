086aa0672ad1dfbfceb804647c54b67051ce3e56
Asad <alviasad2@gmail.com>
2016-08-01 22:36:05 +0530

    boot at stock speeds to prevent boot failure.

--- drivers/cpufreq/freq_table.c
+++ drivers/cpufreq/freq_table.c
@@ -43,9 +43,9 @@ int cpufreq_frequency_table_cpuinfo(struct cpufreq_policy *policy,
 	policy->min = policy->cpuinfo.min_freq = min_freq;
 	policy->max = policy->cpuinfo.max_freq = max_freq;
 
-	/* reset policy at Custom speeds */
-	policy->max = 1000000;
-	policy->min = 100000;
+	/* reset policy at stock speeds */
+	policy->max = 1600000;
+	policy->min = 250000;
 
 	if (policy->min == ~0)
 		return -EINVAL;
