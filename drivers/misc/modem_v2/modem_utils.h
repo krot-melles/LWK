969385602866fd80a53add280d3257554452621c
asad007 <alviasad2@gmail.com>
2016-08-05 05:33:54 -0700

    Misc : Make compile Work

--- drivers/misc/modem_v2/modem_utils.h
+++ drivers/misc/modem_v2/modem_utils.h
@@ -156,15 +156,16 @@ int pr_ipc(const char *str, const char *data, size_t len);
 int pr_buffer(const char *tag, const char *data, size_t data_len,
 							size_t max_len);
 
-/* print a sk_buff as hex string */
+// print a sk_buff as hex string
 #define pr_skb(tag, skb) 
 //	pr_buffer(tag, (char *)((skb)->data), (size_t)((skb)->len), (size_t)16)
 
-/* print a urb as hex string */
+// print a urb as hex string
 #define pr_urb(tag, urb) 
 //	pr_buffer(tag, (char *)((urb)->transfer_buffer), \
-//			(size_t)((urb)->actual_length), (size_t)16)
- 
+			(size_t)((urb)->actual_length), (size_t)16)
+
+
 /* flow control CMD from CP, it use in serial devices */
 int link_rx_flowctl_cmd(struct link_device *ld, const char *data, size_t len);
 
