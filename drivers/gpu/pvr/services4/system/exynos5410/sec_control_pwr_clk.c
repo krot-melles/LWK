692d7a3ca132212f825214a3cea118861299db34
Asad007 <alviasad2@gmail.com>
2016-08-12 07:22:36 -0700

    GPU : Decrease QOS cpu Freq And Enable Wakeup lock state

--- drivers/gpu/pvr/services4/system/exynos5410/sec_control_pwr_clk.c
+++ drivers/gpu/pvr/services4/system/exynos5410/sec_control_pwr_clk.c
@@ -31,7 +31,7 @@
 #define SGX_DEFAULT_CLOCK   350
 #define SGX_DEFAULT_VOLTAGE 925000
 #define WAKEUP_LOCK_CLOCK   266
-#define WAKEUP_LOCK_VOLTAGE 900000
+#define WAKEUP_LOCK_VOLTAGE 850000
 
 #if defined(CONFIG_ARM_EXYNOS5410_BUS_DEVFREQ)
 static struct pm_qos_request exynos5_g3d_cpu_qos;
@@ -43,7 +43,7 @@ module_param_named(qos_mif2_threshold, MIF_THRESHHOLD_VALUE_CLK, uint, S_IWUSR |
 
 static int sec_gpu_top_clock;
 static int gpu_voltage_margin;
-int sec_wakeup_lock_state = 0;
+int sec_wakeup_lock_state = 1;
 bool sec_gpu_power_on = false;
 
 module_param(sec_wakeup_lock_state, int, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
@@ -54,7 +54,7 @@ int sec_gpu_setting_voltage = SGX_DEFAULT_VOLTAGE;
 
 static DEFINE_MUTEX(lock);
 
-static unsigned int qos_cpu_freq = 600000;
+static unsigned int qos_cpu_freq = 400000;
 static unsigned int qos_mif_freq = 200000;
 static unsigned int qos_mif2_freq = 400000;
 static unsigned int qos_int_freq = 200000;
