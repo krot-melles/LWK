f1218897e7afe8a24e9245d18d84ea1ed5d8a479
Asad007 <alviasad2@gmail.com>
2016-08-24 12:48:18 -0700

    removed elementalx governor Needs kgsl.h header file (probably would not work)

--- drivers/cpufreq/cpufreq_barry_allen.c
+++ drivers/cpufreq/cpufreq_barry_allen.c
@@ -342,7 +342,7 @@ static u64 update_load(int cpu)
 	unsigned int delta_time;
 	u64 active_time;
 
-	now_idle = get_cpu_idle_time(cpu, &now, io_is_busy, 0);
+	now_idle = get_cpu_idle_time(cpu, &now, io_is_busy);
 	delta_idle = (unsigned int)(now_idle - pcpu->time_in_idle);
 	delta_time = (unsigned int)(now - pcpu->time_in_idle_timestamp);
 
