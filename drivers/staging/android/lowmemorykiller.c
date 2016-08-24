95e07ca1842a2b8dc581fc9d1d201c34f68d382f
Asad007 <alviasad2@gmail.com>
2016-08-24 12:39:35 -0700

    LMK: Use aggresive values

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -135,11 +135,11 @@ static int lowmem_adj[6] = {
 };
 static int lowmem_adj_size = 6;
 static int lowmem_minfree[6] = {
-	 3 *  512,	/* Foreground App: 	6 MB	*/
-	 2 * 1024,	/* Visible App: 	8 MB	*/
-	 4 * 1024,	/* Secondary Server: 	16 MB	*/
+	 2 * 1024,	/* Foreground App: 	8 MB	*/
+	 4 * 1024,	/* Visible App: 	16 MB	*/
+	 8 * 1024,	/* Secondary Server: 	32 MB	*/
 	16 * 1024,	/* Hidden App: 		64 MB	*/
-	28 * 1024,	/* Content Provider: 	112 MB	*/
+	24 * 1024,	/* Content Provider: 	96 MB	*/
 	32 * 1024,	/* Empty App: 		128 MB	*/
 };
 static int lowmem_minfree_size = 6;
