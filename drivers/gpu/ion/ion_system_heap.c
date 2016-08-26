d8004b666f51121ad570e784b6db98116883a466
Asad007 <alviasad2@gmail.com>
2016-08-26 03:17:32 -0700

    gpu: ion: system_heap: add support for 2MB allocations HIGHLY EXPERIMENTAL

--- drivers/gpu/ion/ion_system_heap.c
+++ drivers/gpu/ion/ion_system_heap.c
@@ -37,7 +37,7 @@ static unsigned int low_order_gfp_flags  = (GFP_HIGHUSER |
 static unsigned int low_order_frag_gfp_flags  = (GFP_HIGHUSER |
 					 __GFP_NOWARN | __GFP_NO_KSWAPD) & ~GFP_IOFS;
 #endif
-static const unsigned int orders[] = {4, 1, 0};
+static const unsigned int orders[] = {9, 8, 4, 0};
 static const int num_orders = ARRAY_SIZE(orders);
 static int order_to_index(unsigned int order)
 {
