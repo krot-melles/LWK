82588b1b4a5b32ac8373b36d991428fd7e7ba8ca
asad007 <alviasad2@gmail.com>
2016-08-30 15:05:05 -0700

    add back vibration control only for AOSP

--- drivers/motor/max77803_haptic.c
+++ drivers/motor/max77803_haptic.c
@@ -2,6 +2,8 @@
  * haptic motor driver for max77803 - max77673_haptic.c
  *
  * Copyright (C) 2011 ByungChang Cha <bc.cha@samsung.com>
+ * Copyright (C) 2012 The CyanogenMod Project
+ *                    Daniel Hillenbrand <codeworkx@cyanogenmod.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
@@ -23,7 +25,11 @@
 #include <linux/mfd/max77803-private.h>
 
 #define TEST_MODE_TIME 10000
-
+#if defined(CONFIG_TW)
+#else
+static unsigned long pwm_val = 50; /* duty in percent */
+static int pwm_duty = 27787; /* duty value, 37050=100%, 27787=50%, 18525=0% */
+#endif
 struct max77803_haptic_data {
 	struct max77803_dev *max77803;
 	struct i2c_client *i2c;
@@ -155,8 +161,13 @@ static void haptic_work(struct work_struct *work)
 
 		max77803_haptic_i2c(hap_data, true);
 
+#if defined(CONFIG_TW)
 		pwm_config(hap_data->pwm, hap_data->pdata->duty,
 			   hap_data->pdata->period);
+#else
+		pwm_config(hap_data->pwm, pwm_duty, hap_data->pdata->period);
+		pr_debug("[VIB] %s: pwm_config duty=%d\n", __func__, pwm_duty);
+#endif
 		pwm_enable(hap_data->pwm);
 
 		if (hap_data->pdata->motor_en)
@@ -234,13 +245,22 @@ void vibtonz_pwm(int nForce)
 {
 	/* add to avoid the glitch issue */
 	static int prev_duty;
+#if defined(CONFIG_TW)
 	int pwm_period = 0, pwm_duty = 0;
+#else
+	int pwm_period = 0;
+#endif
 
 	if (g_hap_data == NULL) {
+#if defined(CONFIG_TW)
 		printk(KERN_ERR "[VIB] the motor is not ready!!!");
+#else
+		pr_err("[VIB] %s: the motor is not ready!!!", __func__);
+#endif
 		return ;
 	}
 
+#if defined(CONFIG_TW)
 	pwm_period = g_hap_data->pdata->period;
 	pwm_duty = pwm_period / 2 + ((pwm_period / 2 - 2) * nForce) / 127;
 
@@ -248,14 +268,65 @@ void vibtonz_pwm(int nForce)
 		pwm_duty = g_hap_data->pdata->duty;
 	else if (pwm_period - pwm_duty > g_hap_data->pdata->duty)
 		pwm_duty = pwm_period - g_hap_data->pdata->duty;
+#endif
 
 	/* add to avoid the glitch issue */
 	if (prev_duty != pwm_duty) {
 		prev_duty = pwm_duty;
+#if defined(CONFIG_TW)
+#else
+	pr_debug("[VIB] %s: setting pwm_duty=%d", __func__, pwm_duty);
+#endif
 		pwm_config(g_hap_data->pwm, pwm_duty, pwm_period);
 	}
 }
 EXPORT_SYMBOL(vibtonz_pwm);
+#endif
+
+#if defined(CONFIG_TW)
+#else
+static ssize_t pwm_value_show(struct device *dev,
+		struct device_attribute *attr, char *buf)
+{
+	int count;
+
+	pwm_val = ((pwm_duty - 18525) * 100) / 18525;
+
+	count = sprintf(buf, "%lu\n", pwm_val);
+	pr_debug("[VIB] pwm_val: %lu\n", pwm_val);
+
+	return count;
+}
+
+ssize_t pwm_value_store(struct device *dev,
+		struct device_attribute *attr,
+		const char *buf, size_t size)
+{
+	if (kstrtoul(buf, 0, &pwm_val))
+		pr_err("[VIB] %s: error on storing pwm_val\n", __func__);
+
+	pr_info("[VIB] %s: pwm_val=%lu\n", __func__, pwm_val);
+
+	pwm_duty = (pwm_val * 18525) / 100 + 18525;
+
+	/* make sure new pwm duty is in range */
+	if(pwm_duty > 37050)
+	{
+		pwm_duty = 37050;
+	}
+	else if (pwm_duty < 18525)
+	{
+		pwm_duty = 18525;
+	}
+
+	pr_info("[VIB] %s: pwm_duty=%d\n", __func__, pwm_duty);
+
+	return size;
+}
+
+static DEVICE_ATTR(pwm_value, S_IRUGO | S_IWUSR,
+		pwm_value_show, pwm_value_store);
+
 #endif
 
 static int max77803_haptic_probe(struct platform_device *pdev)
@@ -329,6 +400,15 @@ static int max77803_haptic_probe(struct platform_device *pdev)
 		error = -EFAULT;
 		goto err_timed_output_register;
 	}
+
+#if defined(CONFIG_TW)
+#else
+	/* User controllable pwm level */
+	error = device_create_file(hap_data->tout_dev.dev, &dev_attr_pwm_value);
+	if (error > 0) {
+		pr_err("[VIB] create sysfs fail: pwm_value\n");
+	}
+#endif
 #endif
 
 	pr_debug("[VIB] -- %s\n", __func__);
