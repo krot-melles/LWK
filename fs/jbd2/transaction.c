eda6f45a5c170a64de3694e4f35f4990e6f4235a
asad007 <alviasad2@gmail.com>
2016-11-23 07:00:59 -0800

    3.4.113

--- fs/jbd2/transaction.c
+++ fs/jbd2/transaction.c
@@ -1899,6 +1899,7 @@ static int journal_unmap_buffer(journal_t *journal, struct buffer_head *bh)
 
 		if (!buffer_dirty(bh)) {
 			/* bdflush has written it.  We can drop it now */
+			__jbd2_journal_remove_checkpoint(jh);
 			goto zap_buffer;
 		}
 
@@ -1936,6 +1937,7 @@ static int journal_unmap_buffer(journal_t *journal, struct buffer_head *bh)
 				/* The orphan record's transaction has
 				 * committed.  We can cleanse this buffer */
 				clear_buffer_jbddirty(bh);
+				__jbd2_journal_remove_checkpoint(jh);
 				goto zap_buffer;
 			}
 		}
