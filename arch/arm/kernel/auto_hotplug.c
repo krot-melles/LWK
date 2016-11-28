34c5e39e258ab0e601574819962de3b5ca07e731
asad007 <alviasad2@gmail.com>
2016-11-28 04:59:38 -0800

    Some More

--- arch/arm/kernel/auto_hotplug.c
+++ arch/arm/kernel/auto_hotplug.c
@@ -57,7 +57,7 @@
 /*
  * MIN_SAMPLING_RATE is scaled based on num_online_cpus()
  */
-#define MIN_SAMPLING_RATE	250
+#define MIN_SAMPLING_RATE	200
 
 /*
  * Load defines:
@@ -67,9 +67,9 @@
  * DISABLE is the load at which a CPU is disabled
  * These two are scaled based on num_online_cpus()
  */
-#define ENABLE_ALL_LOAD_THRESHOLD	500
-#define ENABLE_LOAD_THRESHOLD		300
-#define DISABLE_LOAD_THRESHOLD		200
+#define ENABLE_ALL_LOAD_THRESHOLD	200
+#define ENABLE_LOAD_THRESHOLD		150
+#define DISABLE_LOAD_THRESHOLD		120
 
 /* Control flags */
 unsigned char flags;
