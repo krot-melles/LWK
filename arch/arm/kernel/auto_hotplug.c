9f691e075fdce074e4af610bde98e6152d22fe41
Asad <alviasad2@gmail.com>
2017-01-27 15:52:42 +0530

    Optimise hotplug

--- arch/arm/kernel/auto_hotplug.c
+++ arch/arm/kernel/auto_hotplug.c
@@ -52,12 +52,12 @@
  * SAMPLING_PERIODS * MIN_SAMPLING_RATE is the minimum
  * load history which will be averaged
  */
-#define SAMPLING_PERIODS	5
+#define SAMPLING_PERIODS	10
 #define INDEX_MAX_VALUE		(SAMPLING_PERIODS - 1)
 /*
  * MIN_SAMPLING_RATE is scaled based on num_online_cpus()
  */
-#define MIN_SAMPLING_RATE	100
+#define MIN_SAMPLING_RATE	20
 
 /*
  * Load defines:
@@ -67,8 +67,8 @@
  * DISABLE is the load at which a CPU is disabled
  * These two are scaled based on num_online_cpus()
  */
-#define ENABLE_ALL_LOAD_THRESHOLD	370
-#define ENABLE_LOAD_THRESHOLD		250
+#define ENABLE_ALL_LOAD_THRESHOLD	250
+#define ENABLE_LOAD_THRESHOLD		550
 #define DISABLE_LOAD_THRESHOLD		125
 
 /* Control flags */
